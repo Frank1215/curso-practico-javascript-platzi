ac050a4ab022ab16772e1cce56c593b0616e0d1b
Frank1215 <recinos2791@gmail.com>
2022-03-08 15:01:21 -0600

    Realización de calculos de figuras geometricas

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # curso-practico-javascript-platzi
 Es el curso practico de Javascript de Platzi
+
+## Taller #1: Figuras Geometricas
+
+-primer paso: definir las fomulas
+-Segundo: implementar las formulas en javascript
+-Tercer paso: Crear funciones
+-Cuarto paso: Integrar JavasCript con HTML
---
+++ figuras.js
@@ -0,0 +1,59 @@
+
+//codigo del cuadrado
+console.group('Cuadrados');
+const ladoCuadrado = 5;
+console.log('Los lados del cuadrado miden '+ ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado*4;
+console.log('El perimetro del cuadrado es: '+perimetroCuadrado);
+
+const areaCuadrada = ladoCuadrado*ladoCuadrado;
+console.log('El area del cuadrado es:'+areaCuadrada);
+
+console.groupEnd();
+//codigo del triangulo
+
+console.group('Triangulo');
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTriangulo3 = 4;
+
+console.log('Los lados de los triangulos miden: '
++ ladoTriangulo1 
++ ' cm '
++ ladoTriangulo2
++ ' cm '
++ ladoTriangulo3
++ ' cm '
+);
+
+const alturaTriangulo =5.5;
+console.log('La altura del triangulo es: '+alturaTriangulo);
+
+const perimetroTriangulo = ladoTriangulo1+ladoTriangulo2+ladoTriangulo3;
+console.log('El perimetro del triangulo es: '+perimetroTriangulo);
+
+const areaTriangulo = (ladoTriangulo3* alturaTriangulo)/2;
+console.log('El area del Triangulo es :'+areaTriangulo);
+
+console.groupEnd();
+
+//codigo del circulo
+console.group('Circulos');
+//radio
+const radioCirculo = 4;
+console.log('El radio del circulo es: '+ radioCirculo);
+//diametro
+const diametroCirculo = radioCirculo*2;
+console.log('El diametro del Circulo es : '+ diametroCirculo);
+//PI
+const pI = Math.PI;
+console.log('El pi es: '+ pI);
+//circunferencia
+const perimetroCirculo = diametroCirculo *pI;
+console.log('EL perimetro del Circulo es: '+ perimetroCirculo);
+//Area
+const areaCirculo = (radioCirculo*radioCirculo)*pI;
+console.log('El area del Circulo es: '+ areaCirculo);
+
+console.groupEnd();
