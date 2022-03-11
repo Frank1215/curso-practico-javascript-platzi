456a1a77098235efdab45e0a3bdecb390fbe79ff
Frank1215 <recinos2791@gmail.com>
2022-03-11 12:48:55 -0600

    Funcionalidades de realización de mediana javascript

--- descuentos.js
+++ descuentos.js
@@ -2,10 +2,16 @@
 function calcularPrecioConDescuento (){
    
    const precio     = document.getElementById('idPrecio').value;
-   const descuento  = document.getElementById('idDescuento').value;
+   const descuento  = document.getElementById('inputCoupon').value;
 
     precioCon = (precio * (100-descuento))/100
 
     const parrafo = document.getElementById('parrafoPrecio');
     parrafo.innerText = 'El precio con descuento es: '+ precioCon;
 }
+
+const coupons = [
+    'JuanDC_es_Batman',
+    'Pero_no_le_digas_a_nadie',
+    'es_un_secreto',
+];
